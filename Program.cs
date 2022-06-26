b3d991018b7ca68fc0e09ad9e5b5ea503270d57d
Yung.Energy <morskojroma@gmail.com>
2022-06-26 16:17:00 +0300

    Tast1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Max= " + a + " Min= " + b);
+}
+else
+{
+    Console.WriteLine("Max= " + b + " Min= " + a);
+}
